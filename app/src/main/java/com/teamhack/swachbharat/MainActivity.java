8c93f0a5aec51b3b547716bdf6526bbbcbd1e48f
Rishi Sharma <rishisharma7361@gmail.com>
2016-12-08 13:59:15 +0530

    Basic Project Structure

--- app/src/main/java/com/teamhack/swachbharat/MainActivity.java
+++ app/src/main/java/com/teamhack/swachbharat/MainActivity.java
@@ -3,6 +3,8 @@ package com.teamhack.swachbharat;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,14 +14,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.teamhack.swachbharat.Feed.FeedFragment;
+import com.teamhack.swachbharat.Profile.ProfileFragment;
+import com.teamhack.swachbharat.Share.ShareFragment;
+import com.teamhack.swachbharat.Social.SocialFragment;
+import com.teamhack.swachbharat.Statistics.StatisticsFragment;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    TextView txt_nav_username,txt_nav_email;
+    FirebaseUser firebaseUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        firebaseUser= FirebaseAuth.getInstance().getCurrentUser();
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -40,6 +57,11 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        txt_nav_email= (TextView) navigationView.getHeaderView(0).findViewById(R.id.txt_nav_email);
+        txt_nav_username= (TextView) navigationView.getHeaderView(0).findViewById(R.id.txt_nav_username);
+        txt_nav_username.setText(firebaseUser.getDisplayName());
+        txt_nav_email.setText(firebaseUser.getEmail());
     }
 
     @Override
@@ -80,18 +102,34 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        Class fragmentClass=null;
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
+        if (id == R.id.nav_feed) {
+            fragmentClass= FeedFragment.class;
         } else if (id == R.id.nav_share) {
+            fragmentClass= ShareFragment.class;
+        } else if (id == R.id.nav_social) {
+            fragmentClass= SocialFragment.class;
+        } else if (id == R.id.nav_statistics) {
+            fragmentClass= StatisticsFragment.class;
+        } else if (id == R.id.nav_connect) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_profile) {
+            fragmentClass= ProfileFragment.class;
+        }
 
+        if(fragmentClass!=null){
+
+            try {
+                FragmentTransaction transaction=getSupportFragmentManager().beginTransaction();
+                Fragment fragment= (Fragment) fragmentClass.newInstance();
+                transaction.replace(R.id.content_main,fragment);
+                transaction.commit();
+            } catch (InstantiationException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
