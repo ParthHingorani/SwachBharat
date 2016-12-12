6571d5036ef7202ec6bfb6e35793a5d771c29268
Partha <hingorani.parth@gmail.com>
2016-12-12 13:13:24 +0530

    Update ConnectFragment.java

--- app/src/main/java/com/teamhack/swachbharat/Connect/ConnectFragment.java
+++ app/src/main/java/com/teamhack/swachbharat/Connect/ConnectFragment.java
@@ -26,5 +26,49 @@ public class ConnectFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_connect, container, false);
     }
+    
+    TextView news,a1,a2,a3,a4,a5,a6,r1,r2,r3,r4,v1,v2,v3,v4,v5,join;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        news = (TextView) findViewById(R.id.news1);
+        a1 = (TextView) findViewById(R.id.iAuth1);
+        a2 = (TextView) findViewById(R.id.iAuth2);
+        a3 = (TextView) findViewById(R.id.iAuth3);
+        a4 = (TextView) findViewById(R.id.iAuth4);
+        a5 = (TextView) findViewById(R.id.iAuth5);
+        a6 = (TextView) findViewById(R.id.iAuth6);
+        r1 = (TextView) findViewById(R.id.iArt1);
+        r2 = (TextView) findViewById(R.id.iArt2);
+        r3 = (TextView) findViewById(R.id.iArt3);
+        r4 = (TextView) findViewById(R.id.iArt4);
+        v1 = (TextView) findViewById(R.id.iVideo1);
+        v2 = (TextView) findViewById(R.id.iVideo2);
+        v3 = (TextView) findViewById(R.id.iVideo3);
+        v4 = (TextView) findViewById(R.id.iVideo4);
+        v5 = (TextView) findViewById(R.id.iVideo5);
+        join = (TextView) findViewById(R.id.follow1);
+
+        news.setMovementMethod(LinkMovementMethod.getInstance());
+        a1.setMovementMethod(LinkMovementMethod.getInstance());
+        a2.setMovementMethod(LinkMovementMethod.getInstance());
+        a3.setMovementMethod(LinkMovementMethod.getInstance());
+        a4.setMovementMethod(LinkMovementMethod.getInstance());
+        a5.setMovementMethod(LinkMovementMethod.getInstance());
+        a6.setMovementMethod(LinkMovementMethod.getInstance());
+        r1.setMovementMethod(LinkMovementMethod.getInstance());
+        r2.setMovementMethod(LinkMovementMethod.getInstance());
+        r3.setMovementMethod(LinkMovementMethod.getInstance());
+        r4.setMovementMethod(LinkMovementMethod.getInstance());
+        v1.setMovementMethod(LinkMovementMethod.getInstance());
+        v2.setMovementMethod(LinkMovementMethod.getInstance());
+        v3.setMovementMethod(LinkMovementMethod.getInstance());
+        v4.setMovementMethod(LinkMovementMethod.getInstance());
+        v5.setMovementMethod(LinkMovementMethod.getInstance());
+        join.setMovementMethod(LinkMovementMethod.getInstance());
+    }
 
 }
