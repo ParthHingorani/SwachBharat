f4aaf8bbd6691632ec19491328d721dce1bb6d57
Partha <hingorani.parth@gmail.com>
2016-12-12 13:48:52 +0530

    Update ConnectFragment.java

--- app/src/main/java/com/teamhack/swachbharat/Connect/ConnectFragment.java
+++ app/src/main/java/com/teamhack/swachbharat/Connect/ConnectFragment.java
@@ -4,6 +4,7 @@ package com.teamhack.swachbharat.Connect;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.text.method.LinkMovementMethod;
 import android.view.View;
 import android.view.ViewGroup;
 
