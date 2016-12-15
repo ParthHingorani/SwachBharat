0e776ff1cc862daa2da71e8f4f9ab23333895c74
Partha <hingorani.parth@gmail.com>
2016-12-15 23:05:01 +0530

    Add files via upload

---
+++ app/src/main/java/com/teamhack/swachbharat/Social/SocialDialog.java
@@ -0,0 +1,24 @@
+package com.teamhack.swachbharat.Social;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.view.View;
+
+/**
+ * Created by ParthHingorani on 15-12-2016.
+ */
+
+public class SocialDialog extends Dialog implements View.OnClickListener {
+
+    Context context;
+
+    public SocialDialog(Context context) {
+        super(context);
+        this.context=context;
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
