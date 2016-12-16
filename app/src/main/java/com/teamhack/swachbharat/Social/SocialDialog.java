ed1c623834b5722d8913bc9fb25948783e0c8ad3
Partha <hingorani.parth@gmail.com>
2016-12-16 18:35:16 +0530

    Update SocialDialog.java

--- app/src/main/java/com/teamhack/swachbharat/Social/SocialDialog.java
+++ app/src/main/java/com/teamhack/swachbharat/Social/SocialDialog.java
@@ -1,8 +1,14 @@
 package com.teamhack.swachbharat.Social;
 
 import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+
+import com.teamhack.swachbharat.R;
 
 /**
  * Created by ParthHingorani on 15-12-2016.
@@ -11,6 +17,10 @@ import android.view.View;
 public class SocialDialog extends Dialog implements View.OnClickListener {
 
     Context context;
+    private final static String SOCIAL_CHILD="Social";
+    private Button bt_post,bt_cancel;
+    private ProgressDialog progressDialog;
+    private Social social;
 
     public SocialDialog(Context context) {
         super(context);
@@ -18,7 +28,29 @@ public class SocialDialog extends Dialog implements View.OnClickListener {
     }
 
     @Override
-    public void onClick(View view) {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.dialog_social);
+        bt_post= (Button) findViewById(R.id.bt_feed_dialog_post);
+        bt_cancel= (Button) findViewById(R.id.bt_feed_dialog_cancel);
+        bt_cancel.setOnClickListener(this);
+        bt_post.setOnClickListener(this);
 
     }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.bt_feed_dialog_post:
+                //writeFeed();
+                break;
+            case R.id.bt_feed_dialog_cancel:
+                dismiss();
+                break;
+        }
+
+    }
+
+
 }
